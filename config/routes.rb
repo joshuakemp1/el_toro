35248608d1693f0ca89225caf2b23a2414263b94
Joshua Kemp <josh.kemp@ziplist.com>
2013-12-15 09:00:55 -0500

    Configuring thr routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 ElToro::Application.routes.draw do
-  get "pages/home"
+  root :to => 'pages#home'
 
-  get "pages/about"
+  get "home", :to => "pages#home"
 
-  get "pages/lunch"
+  get "about", :to => "pages#about"
 
-  get "pages/dinner"
+  get "lunch", :to => "pages#lunch"
 
-  get "pages/drinks"
+  get "dinner", :to => "pages#dinner"
 
-  get "pages/gallery"
+  get "drinks", :to => "pages#drinks"
+
+  get "gallery", :to => "pages#gallery"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  #   match 'products#:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  #   match 'products#:id#purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
@@ -53,18 +55,18 @@ ElToro::Application.routes.draw do
 
   # Sample resource route within a namespace:
   #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
+  #     # Directs #admin#products#* to Admin::ProductsController
+  #     # (app#controllers#admin#products_controller.rb)
   #     resources :products
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # just remember to delete public#index.html.
+
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # match ':controller(#:action(#:id))(.:format)'
 end
