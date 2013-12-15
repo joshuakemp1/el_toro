4e555541232e51170a6276d5fee40fad8a5445a2
Joshua Kemp <josh.kemp@ziplist.com>
2013-12-15 08:48:00 -0500

    Generating basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElToro::Application.config.secret_token = '7e2df215b806a25ed19ef4a7bb4ec9055cce8958990402eb743b484e2de9830fc0e96b4952c0771e1c1e06d0ec066154070f211ceb5924a46e4406627941cc6e'
